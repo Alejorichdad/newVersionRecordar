8b0d1be0119481eab9fb8a6b50e63efef128a959
Alejorichdad <topplanner.com.co@gmail.com>
2016-08-20 17:57:52 -0500

    En este commit se actualiza barranquiila
    Se ajusta el content Barranquilla

--- app/src/main/java/com/clubrecordar/recordar2016/helpers/cities/ContentBarranquilla.java
+++ app/src/main/java/com/clubrecordar/recordar2016/helpers/cities/ContentBarranquilla.java
@@ -13,65 +13,65 @@ public class ContentBarranquilla {
 
     public static List<BarranquillaModel> contentBarranquilla =  new ArrayList<>();
 
-    public static String titulo1 = "item title barranquilla 1";
-    public static int imageFile1 = R.drawable.tienda;
-    public static String description1 = "item description barranquilla 1";
+    public static String titulo1 = "Spa Lily Tovar";
+    public static int imageFile1 = R.drawable.lily1;
+    public static String description1 = "Centro integral de belleza";
 
-    public static String titulo2 = "item title barranquilla 2";
-    public static int imageFile2 = R.drawable.tienda;
-    public static String description2 = "item description barranquilla 2";
+    public static String titulo2 = "Directorio de Barranquilla";
+    public static int imageFile2 = R.drawable.sec2;
+    public static String description2 = "Primer guia comercial de la ciudad";
 
-    public static String titulo3 = "item title barranquilla 3";
-    public static int imageFile3 = R.drawable.tienda;
-    public static String description3 = "item description barranquilla 3";
+    public static String titulo3 = "Divercity ";
+    public static int imageFile3 = R.drawable.diver;
+    public static String description3 = "Una ciudad divertida";
 
-    public static String titulo4 = "item title barranquilla 4";
-    public static int imageFile4 = R.drawable.tienda;
-    public static String description4 = "item description barranquilla 4";
+    public static String titulo4 = "Academia del Caribe";
+    public static int imageFile4 = R.drawable.aca;
+    public static String description4 = "Academia de Artes del Caribe";
 
-    public static String titulo5 = "item title barranquilla 5";
-    public static int imageFile5 = R.drawable.tienda;
-    public static String description5 = "item description barranquilla 5";
+    public static String titulo5 = "Politecnico Costa Atlantica";
+    public static int imageFile5 = R.drawable.poli;
+    public static String description5 = "Institución de educación superior";
 
-    public static String titulo6 = "item title barranquilla 6";
-    public static int imageFile6 = R.drawable.tienda;
-    public static String description6 = "item description barranquilla 6";
+    public static String titulo6 = "Auditorías especializadas";
+    public static int imageFile6 = R.drawable.wicer;
+    public static String description6 = "Profesionales especializados en el área de auditoría, ";
 
-    public static String titulo7 = "item title barranquilla 7";
-    public static int imageFile7 = R.drawable.tienda;
-    public static String description7 = "item description barranquilla 7";
+    public static String titulo7 = "Colegio Inca";
+    public static int imageFile7 = R.drawable.inca;
+    public static String description7 = "Primaria y bachillerato diversificado";
 
-    public static String titulo8 = "item title barranquilla 8";
-    public static int imageFile8 = R.drawable.tienda;
-    public static String description8 = "item description barranquilla 8";
+    public static String titulo8 = "Centro Inca";
+    public static int imageFile8 = R.drawable.centroinc;
+    public static String description8 = "Tu camino al éxito";
 
-    public static String titulo9 = "item title barranquilla 9";
-    public static int imageFile9 = R.drawable.tienda;
-    public static String description9 = "item description barranquilla 9";
+    public static String titulo9 = "Indoamérica";
+    public static int imageFile9 = R.drawable.indo;
+    public static String description9 = "Educación superior profesional tecnológica";
 
-    public static String titulo10 = "item title barranquilla 10";
-    public static int imageFile10 = R.drawable.tienda;
-    public static String description10 = "item description barranquilla 10";
+    public static String titulo10 = "Bachillerato técnico autodirigido";
+    public static int imageFile10 = R.drawable.bachiinca;
+    public static String description10 = "Bachillerato técnico Inca";
 
-    public static String titulo11 = "item title barranquilla 11";
-    public static int imageFile11 = R.drawable.tienda;
-    public static String description11 = "item description barranquilla 11";
+    public static String titulo11 = "Prado mar hotel";
+    public static int imageFile11 = R.drawable.prado;
+    public static String description11 = "El Hotel Pradomar ubicado en Puerto Colombia";
 
-    public static String titulo12 = "item title barranquilla 12";
-    public static int imageFile12 = R.drawable.tienda;
-    public static String description12 = "item description barranquilla 12";
+    public static String titulo12 = "Electro Aceite";
+    public static int imageFile12 = R.drawable.electro;
+    public static String description12 = "Repuestos para automotores";
 
-    public static String titulo13 = "item title barranquilla 13";
-    public static int imageFile13 = R.drawable.tienda;
-    public static String description13 = "item description barranquilla 13";
+    public static String titulo13 = "Novasys de Colombia";
+    public static int imageFile13 = R.drawable.novasys;
+    public static String description13 = "Alquiler de computadores de oficina";
 
-    public static String titulo14 = "item title barranquilla 14";
-    public static int imageFile14 = R.drawable.tienda;
-    public static String description14 = "item description barranquilla 14";
+    public static String titulo14 = "Club Deportivo Instenalco";
+    public static int imageFile14 = R.drawable.instenalco;
+    public static String description14 = "Somos Parte de Tú Desarrollo ";
 
-    public static String titulo15 = "item title barranquilla 15";
-    public static int imageFile15 = R.drawable.tienda;
-    public static String description15 = "item description barranquilla 15";
+    public static String titulo15 = "Parque del Sol";
+    public static int imageFile15 = R.drawable.parquesol;
+    public static String description15 = "Contamos con zonas de esparcimiento familiar";
 
     public static void addContentBarranquilla(){
         contentBarranquilla.add(new BarranquillaModel(imageFile1, titulo1, description1));
